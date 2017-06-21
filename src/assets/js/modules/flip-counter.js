82b4a267b808cecc6a903d10db6c71e69bae943c
Matine Chabrier <matine.chabrier@pod-point.com>
2017-06-21 15:00:43 +0100

    eslint flip-counter.js bar 2 errors saying Tick is not defined??

--- src/assets/js/modules/flip-counter.js
+++ src/assets/js/modules/flip-counter.js
@@ -1,15 +1,15 @@
-import flip from "../../plugins/flip-counter/flip/tick.js";
-import { nodesToArray, addClass } from '@pod-point/dom-ops';
+import { addClass } from '@pod-point/dom-ops';
+import '../../plugins/flip-counter/flip/tick';
 
-let instances = [];
+const instances = [];
 const LOADED = 'loaded';
 
 class FlipCounter {
 
     /**
-     * Creates a new flip counter element.
+     * Creates a new flip counter element
      *
-     * @param element
+     * @param {element}
      */
     constructor(element) {
         this.element = element;
@@ -17,14 +17,14 @@ class FlipCounter {
     }
 
     /**
-     * Creates a new flip counter element with options.
+     * Creates a new flip counter element with options
      */
     createTickCounter() {
         const element = this.element;
-        let stat = parseInt(element.getAttribute('data-stat'));
+        let stat = parseInt(element.getAttribute('data-stat'), 0);
         const flipCounterSections = document.querySelectorAll('.flip-counter-section');
 
-        var tick = Tick.DOM.create( element, {
+        const tick = Tick.DOM.create(element, {
             value: stat,
             view: {
                 children: [{
@@ -32,27 +32,26 @@ class FlipCounter {
                     layout: 'horizontal',
                     repeat: true,
                     children: [{
-                        view: 'flip'
-                    }]
-                }]
+                        view: 'flip',
+                    }],
+                }],
             },
-            didInit: function() {
-                setTimeout(function(){
+            didInit: () => {
+                setTimeout(() => {
                     flipCounterSections.forEach(item => addClass(item, LOADED));
                 }, 1500);
-            }
+            },
         });
 
-        Tick.helper.interval(function(){
+        Tick.helper.interval(() => {
             stat += Math.round(Math.random());
             tick.value = stat;
         }, 2500);
-
     }
 }
 
 export default {
-    init: function(element) {
+    init: element => {
         instances.push(new FlipCounter(element));
-    }
+    },
 };
