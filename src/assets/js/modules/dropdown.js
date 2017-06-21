686d9cf27f701b76bf19ce14fb4ae90d9d772468
Matine Chabrier <matine.chabrier@pod-point.com>
2017-06-21 14:48:31 +0100

    eslint dropdown.js

--- src/assets/js/modules/dropdown.js
+++ src/assets/js/modules/dropdown.js
@@ -5,57 +5,47 @@ let instances = [];
 class DropDown {
 
     /**
-     * Creates a new drop down element.
+     * Creates a new drop down element
      *
-     * @param element
+     * @param {element}
      */
     constructor(element) {
         this.element = element;
-
         this.bindEvents();
     }
 
     /**
-     * Bind any event listeners to the elements.
+     * Bind any event listeners to the elements
      */
     bindEvents() {
         this.listener = new Delegate(this.element);
 
         this.listener.on('click', (event, input) => {
             event.preventDefault();
-
             this.doDropDown(input);
         });
 
         this.listener.on('blur', (event, input) => {
             event.preventDefault();
-
             this.closeDropDown(input);
         });
     }
 
     /**
-     * Unbinds the event listeners from the elements.
-     */
-    unbindEvents() {
-        this.listener.destroy();
-    }
-
-    /**
-     * Handle drop down opening.
+     * Handle drop down opening
      *
-     * @param {Element} input
+     * @param {element} input
      */
-    doDropDown(input) {
+    static doDropDown(input) {
         input.parentElement.classList.toggle('open');
     }
 
     /**
-     * Handle drop down closing.
+     * Handle drop down closing
      *
-     * @param {Element} input
+     * @param {element} input
      */
-    closeDropDown(input) {
+    static closeDropDown(input) {
         input.parentElement.classList.remove('open');
 
         // Trigger the click event on the target if it not opening another menu
@@ -64,15 +54,21 @@ class DropDown {
         }
     }
 
+    /**
+     * Unbinds the event listeners from the elements
+     */
+    unbindEvents() {
+        this.listener.destroy();
+    }
 }
 
 export default {
-    init: function(element) {
+    init: element => {
         instances.push(new DropDown(element));
     },
 
-    destroy: function() {
-        instances.forEach((instance) => instance.unbindEvents());
+    destroy: () => {
+        instances.forEach(instance => instance.unbindEvents());
         instances = [];
-    }
+    },
 };
