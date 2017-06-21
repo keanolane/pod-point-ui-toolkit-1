9407b6e7dd6ea3ac281f7015fc418241f0448459
Matine Chabrier <matine.chabrier@pod-point.com>
2017-06-21 16:24:01 +0100

    eslint toggle-accordion-panel.js

--- src/assets/js/modules/toggle-accordion-panel.js
+++ src/assets/js/modules/toggle-accordion-panel.js
@@ -1,5 +1,5 @@
 import { Delegate } from 'dom-delegate';
-import { nodesToArray, hasClass, addClass, removeClass } from '@pod-point/dom-ops';
+import { nodesToArray, hasClass } from '@pod-point/dom-ops';
 import { openPanel, closePanel } from './../utilities';
 
 let instances = [];
@@ -8,35 +8,45 @@ const IS_OPEN = 'is-open';
 class ToggleAccordionPanel {
 
     /**
-     * Creates a new toggle panel.
+     * Creates a new toggle panel
      *
-     * @param element
+     * @param {element}
      */
     constructor(element) {
+        // Selectors
+        const toggleIconSelector = `[data-toggle-icon="${this.panelId}"]`;
+        const toggleButtonsSelector = `[data-toggle-panel="${this.panelId}"]`;
+        const openButtonsSelector = `[data-open-panel="${this.panelId}"]`;
+        const closeButtonsSelector = `[data-close-panel="${this.panelId}"]`;
+        const radioOpenButtonsSelector = `[data-radio-open-panel="${this.panelId}"]`;
+        const radioCloseButtonsSelector = `[data-radio-close-panel="${this.panelId}"]`;
+        const inputOpenButtonsSelector = `[data-input-open-panel="${this.panelId}"]`;
+        const selectToggleButtonsSelector = `[data-select-toggle-panel="${this.panelId}"]`;
+
         this.panel = element;
         this.panelId = element.getAttribute('id');
-        this.toggleIcon = document.querySelector('[data-toggle-icon="'+this.panelId+'"]')
-        this.toggleButtons = nodesToArray(document.querySelectorAll('[data-toggle-panel="'+this.panelId+'"]')) || [];
-        this.openButtons = nodesToArray(document.querySelectorAll('[data-open-panel="'+this.panelId+'"]')) || [];
-        this.closeButtons = nodesToArray(document.querySelectorAll('[data-close-panel="'+this.panelId+'"]')) || [];
-        this.radioOpenButtons = nodesToArray(document.querySelectorAll('[data-radio-open-panel="'+this.panelId+'"]')) || [];
-        this.radioCloseButtons = nodesToArray(document.querySelectorAll('[data-radio-close-panel="'+this.panelId+'"]')) || [];
-        this.inputOpenButtons = nodesToArray(document.querySelectorAll('[data-input-open-panel="'+this.panelId+'"]')) || [];
+        this.toggleIcon = document.querySelector(toggleIconSelector);
+        this.toggleButtons = nodesToArray(document.querySelectorAll(toggleButtonsSelector)) || [];
+        this.openButtons = nodesToArray(document.querySelectorAll(openButtonsSelector)) || [];
+        this.closeButtons = nodesToArray(document.querySelectorAll(closeButtonsSelector)) || [];
+        this.radioOpenButtons = nodesToArray(document.querySelectorAll(radioOpenButtonsSelector)) || [];
+        this.radioCloseButtons = nodesToArray(document.querySelectorAll(radioCloseButtonsSelector)) || [];
+        this.inputOpenButtons = nodesToArray(document.querySelectorAll(inputOpenButtonsSelector)) || [];
 
-        this.selectToggleButtons = nodesToArray(document.querySelectorAll('[data-select-toggle-panel="'+this.panelId+'"]')) || [];
+        this.selectToggleButtons = nodesToArray(document.querySelectorAll(selectToggleButtonsSelector)) || [];
 
         this.bindEvents();
     }
 
     /**
-     * Binds the event listeners from the elements.
+     * Binds the event listeners from the elements
      */
     bindEvents() {
         this.toggleListeners = [];
         this.toggleButtons.forEach(toggleButton => {
             const toggleListener = new Delegate(toggleButton);
             this.toggleListeners.push(toggleListener);
-            toggleListener.on('click', (event) => {
+            toggleListener.on('click', event => {
                 event.preventDefault();
                 this.togglePanel();
             });
@@ -46,7 +56,7 @@ class ToggleAccordionPanel {
         this.openButtons.forEach(openButton => {
             const openListener = new Delegate(openButton);
             this.openListeners.push(openListener);
-            openListener.on('click', (event) => {
+            openListener.on('click', event => {
                 event.preventDefault();
                 openPanel(this.panel);
             });
@@ -56,7 +66,7 @@ class ToggleAccordionPanel {
         this.closeButtons.forEach(closeButton => {
             const closeListener = new Delegate(closeButton);
             this.closeListeners.push(closeListener);
-            closeListener.on('click', (event) => {
+            closeListener.on('click', event => {
                 event.preventDefault();
                 closePanel(this.panel);
             });
@@ -66,9 +76,9 @@ class ToggleAccordionPanel {
         this.radioOpenButtons.forEach(radioOpenButton => {
             const radioOpenListener = new Delegate(radioOpenButton);
             this.radioOpenListeners.push(radioOpenListener);
-            radioOpenListener.on('change', (event) => {
+            radioOpenListener.on('change', event => {
                 event.preventDefault();
-               openPanel(this.panel);
+                openPanel(this.panel);
             });
         });
 
@@ -76,7 +86,7 @@ class ToggleAccordionPanel {
         this.radioCloseButtons.forEach(radioCloseButton => {
             const radioCloseListener = new Delegate(radioCloseButton);
             this.radioCloseListeners.push(radioCloseListener);
-            radioCloseListener.on('change', (event) => {
+            radioCloseListener.on('change', event => {
                 event.preventDefault();
                 closePanel(this.panel);
             });
@@ -105,21 +115,8 @@ class ToggleAccordionPanel {
         });
     }
 
-
     /**
-     * Unbinds the event listeners from the elements.
-     */
-    unbindEvents() {
-        this.toggleListeners.forEach(toggleListener => toggleListener.destroy());
-        this.openListeners.forEach(openListener => openListener.destroy());
-        this.closeListeners.forEach(closeListener => closeListener.destroy());
-        this.radioOpenListeners.forEach(radioOpenListener => radioOpenListener.destroy());
-        this.radioCloseListeners.forEach(radioCloseListener => radioCloseListener.destroy());
-        this.inputOpenListeners.forEach(inputOpenListener => inputOpenListener.destroy());
-    }
-
-    /**
-     * Toggle panel depending if already open or not.
+     * Toggle panel depending if already open or not
      */
     togglePanel() {
         const panelIsVisible = hasClass(this.panel, IS_OPEN);
@@ -130,15 +127,27 @@ class ToggleAccordionPanel {
             openPanel(this.panel);
         }
     }
+
+    /**
+     * Unbinds the event listeners from the elements
+     */
+    unbindEvents() {
+        this.toggleListeners.forEach(toggleListener => toggleListener.destroy());
+        this.openListeners.forEach(openListener => openListener.destroy());
+        this.closeListeners.forEach(closeListener => closeListener.destroy());
+        this.radioOpenListeners.forEach(radioOpenListener => radioOpenListener.destroy());
+        this.radioCloseListeners.forEach(radioCloseListener => radioCloseListener.destroy());
+        this.inputOpenListeners.forEach(inputOpenListener => inputOpenListener.destroy());
+    }
 }
 
 export default {
-    init: function(element) {
+    init: element => {
         instances.push(new ToggleAccordionPanel(element));
     },
 
-    destroy: function() {
-        instances.forEach((instance) => instance.unbindEvents());
+    destroy: () => {
+        instances.forEach(instance => instance.unbindEvents());
         instances = [];
-    }
+    },
 };
