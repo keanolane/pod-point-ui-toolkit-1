2921417bc7d1dc781be78248657c83dae21fb3ce
Matine Chabrier <matine.chabrier@pod-point.com>
2017-06-21 10:26:57 +0100

    eslint script.js

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,9 +1,6 @@
-require('./shims/classList.js');
-
 import loadModules from '@pod-point/module-loader';
 import combineDomModules from '@pod-point/dom-module-loader';
 import * as dom from '@pod-point/dom-ops';
-import {addClass} from '@pod-point/dom-ops';
 
 import base from './modules/base';
 import modal from './modules/modal';
@@ -18,7 +15,6 @@ import gallerySimple from './modules/gallery-simple';
 import accordion from './modules/accordion';
 import headerNav from './modules/header-nav';
 import carousel from './modules/carousel';
-import addressLookup from './modules/address-lookup';
 import changeContent from './modules/change-content';
 import checkoutYourPodPoint from './modules/checkout-your-pod-point';
 import claimOlev from './modules/claim-olev';
@@ -26,12 +22,13 @@ import claimDealerDiscount from './modules/claim-dealer-discount';
 import basket from './modules/basket';
 import flipCounter from './modules/flip-counter';
 import evMap from './modules/ev-map';
+import * as addressLookup from './modules/address-lookup';
 
-import { initAutocomplete, geolocate, fillInAddress } from './modules/address-lookup';
+require('./shims/classList.js');
 
-window.initAutocomplete = initAutocomplete;
-window.geolocate = geolocate;
-window.fillInAddress = fillInAddress;
+window.initAutocomplete = addressLookup.initAutocomplete;
+window.geolocate = addressLookup.geolocate;
+window.fillInAddress = addressLookup.fillInAddress;
 
 dom.whenReady(() => {
     loadModules({
@@ -57,24 +54,24 @@ dom.whenReady(() => {
             basket,
             flipCounter,
             evMap,
-        })
+        }),
     });
 });
 
-function debounce(callback, wait, context = this) {
-    let timeout = null
-    let callbackArgs = null
-    const later = () => callback.apply(context, callbackArgs)
+// function debounce(callback, wait, context = this) {
+//     let timeout = null;
+//     let callbackArgs = null;
+//     const later = () => callback.apply(context, callbackArgs);
 
-    return function() {
-        callbackArgs = arguments
-        clearTimeout(timeout)
-        timeout = setTimeout(later, wait)
-    }
-}
-const handleResize = debounce((e) => {
-    loadModules();
-}, 100)
+//     return () => {
+//         callbackArgs = arguments;
+//         clearTimeout(timeout);
+//         timeout = setTimeout(later, wait);
+//     };
+// }
+// const handleResize = debounce((e) => {
+//     loadModules();
+// }, 100);
 
 // window.addEventListener('resize', handleResize)
 
