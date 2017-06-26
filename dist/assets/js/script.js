fab84818faf9162ba86750571974552018aca549
Matine Chabrier <matine.chabrier@pod-point.com>
2017-06-26 14:20:10 +0100

    Added modernizr gulp build and various IE styling fixes including the grid calc not working and setting widths and heights to inline svgs directly

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -777,11 +777,16 @@ var _moduleLoader = require('@pod-point/module-loader');
 	    var winWidthMedium = 800;
 	    var winWidth = window.innerWidth;
 	    window.isMobileSize = winWidth < winWidthMedium;
+	    var isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
+	    var isIE10 = document.body.style.msTouchAction != undefined;
 	    window.onload = function () {
 	        if (window.isTouchDevice) {
 	            (0, _domOps.addClass)(document.body, 'is-touch');
 	        } else {
 	            (0, _domOps.addClass)(document.body, 'is-desktop');
+	        }
+	        if (isIE11 || isIE10) {
+	            (0, _domOps.addClass)(document.documentElement, 'ie');
 	        }
 	    };
 	};
