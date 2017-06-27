9b1b77e05fbb2e7b3c2fb194662bcdc959b36b26
Matine Chabrier <matine.chabrier@pod-point.com>
2017-06-27 11:17:01 +0100

    Removed IE checks in base module

--- src/assets/js/modules/base.js
+++ src/assets/js/modules/base.js
@@ -1,20 +1,16 @@
 import { addClass } from '@pod-point/dom-ops';
 
 const defineSizeAndDevice = () => {
+    window.isTouchDevice = 'ontouchstart' in document.documentElement;
+    const winWidthMedium = 800;
+    window.isMobileSize = (winWidth < winWidthMedium);
+    const winWidth = window.innerWidth;
+
     window.onload = () => {
-        window.isTouchDevice = 'ontouchstart' in document.documentElement;
-        const winWidthMedium = 800;
-        window.isMobileSize = (winWidth < winWidthMedium);
-        const winWidth = window.innerWidth;
-        const isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
-        const isIE10 = document.body.style.msTouchAction != undefined;
         if (window.isTouchDevice) {
             addClass(document.body, 'is-touch');
         } else {
             addClass(document.body, 'is-desktop');
-        }
-        if (isIE11 || isIE10) {
-            addClass(document.documentElement, 'ie');
         }
     };
 };
