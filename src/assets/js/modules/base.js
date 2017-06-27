9a6ff2c1ad4f098cb20ba46bcdae45647756c5c4
Matine Chabrier <matine.chabrier@pod-point.com>
2017-06-27 10:27:36 +0100

    Moved variables into window.onload function in base midule

--- src/assets/js/modules/base.js
+++ src/assets/js/modules/base.js
@@ -1,13 +1,13 @@
 import { addClass } from '@pod-point/dom-ops';
 
 const defineSizeAndDevice = () => {
-    window.isTouchDevice = 'ontouchstart' in document.documentElement;
-    const winWidthMedium = 800;
-    const winWidth = window.innerWidth;
-    window.isMobileSize = (winWidth < winWidthMedium);
-    const isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
-    const isIE10 = document.body.style.msTouchAction != undefined;
     window.onload = () => {
+        window.isTouchDevice = 'ontouchstart' in document.documentElement;
+        const winWidthMedium = 800;
+        window.isMobileSize = (winWidth < winWidthMedium);
+        const winWidth = window.innerWidth;
+        const isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
+        const isIE10 = document.body.style.msTouchAction != undefined;
         if (window.isTouchDevice) {
             addClass(document.body, 'is-touch');
         } else {
