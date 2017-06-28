b97e4753678454539d538f87d1dfe191b691eeaf
Matine Chabrier <matine.chabrier@pod-point.com>
2017-06-28 15:21:26 +0100

    Made nav icons aligned in size

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -341,7 +341,6 @@
 	
 	module.exports = exports['default'];
 
-
 /***/ }),
 /* 4 */
 /***/ (function(module, exports) {
@@ -4103,7 +4102,7 @@
 /***/ (function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*!
-	 * Flickity v2.0.8
+	 * Flickity v2.0.7
 	 * Touch, responsive, flickable carousels
 	 *
 	 * Licensed GPLv3 for open source use
@@ -6202,10 +6201,20 @@
 	  this.dispatchEvent( 'pointerDown', event, [ pointer ] );
 	};
 	
+	var touchStartEvents = {
+	  touchstart: true,
+	  MSPointerDown: true
+	};
+	
+	var focusNodes = {
+	  INPUT: true,
+	  SELECT: true
+	};
+	
 	proto.pointerDownFocus = function( event ) {
 	  // focus element, if not touch, and its not an input or select
-	  var canPointerDown = getCanPointerDown( event );
-	  if ( !this.options.accessibility || canPointerDown ) {
+	  if ( !this.options.accessibility || touchStartEvents[ event.type ] ||
+	      focusNodes[ event.target.nodeName ] ) {
 	    return;
 	  }
 	  var prevScrollY = window.pageYOffset;
@@ -6216,26 +6225,11 @@
 	  }
 	};
 	
-	var touchStartEvents = {
-	  touchstart: true,
-	  pointerdown: true,
-	};
-	
-	var focusNodes = {
-	  INPUT: true,
-	  SELECT: true,
-	};
-	
-	function getCanPointerDown( event ) {
-	  var isTouchStart = touchStartEvents[ event.type ];
-	  var isFocusNode = focusNodes[ event.target.nodeName ];
-	  return isTouchStart || isFocusNode;
-	}
-	
 	proto.canPreventDefaultOnPointerDown = function( event ) {
-	  // prevent default, unless touchstart or input
-	  var canPointerDown = getCanPointerDown( event );
-	  return !canPointerDown;
+	  // prevent default, unless touchstart or <select>
+	  var isTouchstart = event.type == 'touchstart';
+	  var targetNodeName = event.target.nodeName;
+	  return !isTouchstart && targetNodeName != 'SELECT';
 	};
 	
 	// ----- move ----- //
@@ -6449,7 +6443,7 @@
 /***/ (function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*!
-	 * Unidragger v2.2.2
+	 * Unidragger v2.2.1
 	 * Draggable base class
 	 * MIT license
 	 */
@@ -6515,11 +6509,6 @@
 	    var handle = this.handles[i];
 	    this._bindStartEvent( handle, isBind );
 	    handle[ bindMethod ]( 'click', this );
-	    // touch-action: none to override browser touch gestures
-	    // metafizzy/flickity#540
-	    if ( window.PointerEvent ) {
-	      handle.style.touchAction = isBind ? 'none' : '';
-	    }
 	  }
 	};
 	
